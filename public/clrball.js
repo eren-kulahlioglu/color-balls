3c4b3f207171e1dc158735b0b09b4c555d74c81d
Eren Kulahlioglu <erkulahlioglu@gmail.com>
2020-06-07 21:32:23 +0100

    initial commit

---
+++ public/clrball.js
@@ -0,0 +1,64 @@
+function changeRed() {
+    document.getElementById('ball').style.backgroundColor = "red";
+    document.getElementById('ball1').style.backgroundColor = "red";
+    document.getElementById('ball2').style.backgroundColor = "red";
+
+
+}
+
+function changeBlue() {
+    document.getElementById('ball').style.backgroundColor = "blue";
+    document.getElementById('ball1').style.backgroundColor = "blue";
+    document.getElementById('ball2').style.backgroundColor = "blue";
+
+
+}
+
+function changeYellow() {
+    document.getElementById('ball').style.backgroundColor = "yellow";
+    document.getElementById('ball1').style.backgroundColor = "yellow";
+    document.getElementById('ball2').style.backgroundColor = "yellow";
+
+
+} 
+
+function changeGreen () {
+    document.getElementById('ball').style.backgroundColor = "green";
+    document.getElementById('ball1').style.backgroundColor = "green";
+    document.getElementById('ball2').style.backgroundColor = "green";
+
+
+} 
+
+function changeBrown () {
+    document.getElementById('ball').style.backgroundColor = "brown";
+    document.getElementById('ball1').style.backgroundColor = "brown";
+    document.getElementById('ball2').style.backgroundColor = "brown";
+
+
+} 
+
+function changeOrange () {
+    document.getElementById('ball').style.backgroundColor = "orange";
+    document.getElementById('ball1').style.backgroundColor = "orange";
+    document.getElementById('ball2').style.backgroundColor = "orange";
+
+
+} 
+
+function changeWhite () {
+    document.getElementById('ball').style.backgroundColor = "white";
+    document.getElementById('ball1').style.backgroundColor = "white";
+    document.getElementById('ball2').style.backgroundColor = "white";
+
+
+} 
+
+function changeBlack () {
+    document.getElementById('ball').style.backgroundColor = "black";
+    document.getElementById('ball1').style.backgroundColor = "black";
+    document.getElementById('ball2').style.backgroundColor = "black";
+
+
+} 
+
